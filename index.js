87739aa6b76eaa03cbbcd7b0729e818e93a0de16
Cristian Vianna <jbcristian78@gmail.com>
2025-05-09 16:03:49 -0300

    Adicionando pagina de pesquisa

--- index.js
+++ index.js
@@ -14,9 +14,30 @@ App.get("/", (req, res) => {
     res.render("index")
 });
 
-App.post("/salvar", (req, res) => {
-    var form = req.body;
+App.get("/pesquisas", (req, res) => {
+    database.select().from('pesquisas')
+        .then(data => {
+            res.render("pesquisas", {pesquisa: data})
+        }).catch(err => {console.log(err)})
+       
     
+})
+
+App.post("/salvar", (req, res) => {
+    const form = req.body;
+
+   
+    function temConteudoMalicioso(obj) {
+        const padrao = /[<>{}*$]/;
+        return Object.values(obj).some(valor => typeof valor === "string" && padrao.test(valor));
+    }
+
+    // Verificação
+    if (temConteudoMalicioso(form)) {
+        console.log("⚠️ Tentativa de injeção detectada");
+        return res.status(400).send("Entrada inválida detectada.");
+    }
+
     const dados = {
         nome: form.nome,
         nome_empresa: form.nome_empresa,
@@ -31,18 +52,18 @@ App.post("/salvar", (req, res) => {
         contador: form.contador === 'sim' ? 1 : 0,
         inss: form.inss === 'sim' ? 1 : 0,
         obs: form.observacoes
-    }
-    
+    };
 
-    database.insert(dados).into('pesquisas').then(() => {
-        console.log("Dados adicionados com sucesso")
-        res.redirect("/")
-    }).catch(err => {
-        console.log(err)
-    })
-
-   
-})
+    database.insert(dados).into('pesquisas')
+        .then(() => {
+            console.log("✅ Dados adicionados com sucesso");
+            res.redirect("/");
+        })
+        .catch(err => {
+            console.error("Erro ao inserir no banco:", err);
+            res.status(500).send("Erro interno no servidor.");
+        });
+});
 
 App.listen(8080, () => {
     console.log("conexão feita com sucesso")
