c5976c72666ecb9757202d01e8817494d22e86ed
Cristian Vianna <jbcristian78@gmail.com>
2025-05-10 11:58:05 -0300

    Aficionando porta no knex

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
-
 const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    }
-  });
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // Certifique-se de que a porta também está sendo passada corretamente
+    connectTimeout: 600000 // Aumenta o timeout para 10 segundos
+  }
+});
 
-  
 module.exports = knex;
